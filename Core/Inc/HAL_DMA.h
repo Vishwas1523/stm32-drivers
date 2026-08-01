a0ff977c1ab08c7ebbcec7dff609379f1d06d281
Vishwas Prajapati <vishwas1523@gmail.com>
2026-08-01 23:43:53 +0530

    make DMA init and deinit functions

--- Core/Inc/HAL_DMA.h
+++ Core/Inc/HAL_DMA.h
@@ -21,37 +21,36 @@ typedef enum{
 
 
 typedef enum{
-	P_to_P = 0,
-	M_to_P = 1,
-	M_to_M = 2
+	DMA_DIRECTION_PER_TO_MEM = 0,
+	DMA_DIRECTION_MEM_TO_PER = 1,
+	DMA_DIRECTION_MEM_TO_MEM = 2
 }DMA_Direction_t;
 
 typedef enum{
-	BYTE = 0,
-	HALF_WORD = 1,
-	WORD = 2
+	DMA_DATA_SIZE_BYTE = 0,
+	DMA_DATA_SIZE_HALF_WORD = 1,
+	DMA_DATA_SIZE_WORD = 2
 }DMA_Size_t;
 
 typedef enum{
-	LOw = 0,
-	MEDIUM = 1,
-	HIGH = 2,
-	VERY_HIGH = 3
+	DMA_PRIORITY_LOW = 0,
+	DMA_PRIORITY_MEDIUM = 1,
+	DMA_PRIORITY_HIGH = 2,
+	DMA_PRIORITY_VERY_HIGH = 3
 }DMA_Priority_t;
 
 typedef enum{
-	CHANNEL0 = 0,
-	CHANNEL1 = 1,
-	CHANNEL2 = 2,
-	CHANNEL3 = 3,
-	CHANNEL4 = 4,
-	CHANNEL5 = 5,
-	CHANNEL6 = 6,
-	CHANNEL7 = 7,
+	DMA_CHANNEL0 = 0,
+	DMA_CHANNEL1 = 1,
+	DMA_CHANNEL2 = 2,
+	DMA_CHANNEL3 = 3,
+	DMA_CHANNEL4 = 4,
+	DMA_CHANNEL5 = 5,
+	DMA_CHANNEL6 = 6,
+	DMA_CHANNEL7 = 7,
 }DMA_Channel_t;
 
 typedef struct{
-	uint16_t numOfTransations;
 	DMA_Direction_t direction;
 	DMA_FunctionalState_t circularMode;
 	DMA_FunctionalState_t peripheralIncrementMode;
@@ -60,14 +59,14 @@ typedef struct{
 	DMA_Size_t MSIZE;
 	DMA_Priority_t priority;
 	DMA_Channel_t channel;
-}DMA_Config_t;
+}DMA_Stream_Config_t;
 
 typedef struct{
 	DMA_Stream_TypeDef* instance;
-	DMA_Config_t config;
+	DMA_Stream_Config_t config;
 }DMA_HandlerTypeDef;
 
-
+//------------------------DMA1 Streams and their addresses---------------------
 #define DMA1_Stream0				(DMA_Stream_TypeDef*)0x40026010
 #define DMA1_Stream1				(DMA_Stream_TypeDef*)0x40026028
 #define DMA1_Stream2				(DMA_Stream_TypeDef*)0x40026040
@@ -77,12 +76,34 @@ typedef struct{
 #define DMA1_Stream6				(DMA_Stream_TypeDef*)0x400260A0
 #define DMA1_Stream7				(DMA_Stream_TypeDef*)0x400260B8
 
+//------------------------DMA2 Streams and their addresses---------------------
+#define DMA2_Stream0				(DMA_Stream_TypeDef*)0x40026410
+#define DMA2_Stream1				(DMA_Stream_TypeDef*)0x40026428
+#define DMA2_Stream2				(DMA_Stream_TypeDef*)0x40026440
+#define DMA2_Stream3				(DMA_Stream_TypeDef*)0x40026458
+#define DMA2_Stream4				(DMA_Stream_TypeDef*)0x40026470
+#define DMA2_Stream5				(DMA_Stream_TypeDef*)0x40026488
+#define DMA2_Stream6				(DMA_Stream_TypeDef*)0x400264A0
+#define DMA2_Stream7				(DMA_Stream_TypeDef*)0x400264B8
+
+//------------------------DMA BIT MANIPULATION & MASKING----------------------
+#define DMA_CR_EN					(1<<0)
+#define DMA_CR_CHSEL_Pos			 25UL
+#define DMA_CR_PL_Pos				 16UL
+#define DMA_CR_DIR_Pos				 6UL
+#define DMA_CR_PINC_Pos				 9UL
+#define DMA_CR_MINC_Pos				 10UL
+#define DMA_CR_CIRC_Pos				 8UL
+#define DMA_CR_PSIZE_Pos			 11UL
+#define DMA_CR_MSIZE_Pos			 13UL
 
 
 
 
 
 
+void DMA_Init(DMA_HandlerTypeDef* hdma);
+void DMA_Start(DMA_HandlerTypeDef* hdma, uint32_t srcAddress, uint32_t dstAddress, uint16_t numOfTransfers);
 
 
 
--- Core/Src/HAL_DMA.c
+++ Core/Src/HAL_DMA.c
@@ -4,7 +4,23 @@
  *  Created on: Jul 29, 2026
  *      Author: VICTUS
  */
-#include <HAL_DMA.h>
+#include "HAL_DMA.h"
 
+
+inline void DMA_DeInit(DMA_HandlerTypeDef* hdma){
+	hdma->instance->CR &= ~DMA_CR_EN;
+}
+
+void DMA_Init(DMA_HandlerTypeDef* hdma){
+	DMA_DeInit(hdma);
+	hdma->instance->CR |= hdma->config.channel		<< 	DMA_CR_CHSEL_Pos;
+	hdma->instance->CR |= hdma->config.priority		<<	DMA_CR_PL_Pos;
+	hdma->instance->CR |= hdma->config.direction	<< 	DMA_CR_DIR_Pos;
+	hdma->instance->CR |= hdma->config.peripheralIncrementMode	<< DMA_CR_PINC_Pos;
+	hdma->instance->CR |= hdma->config.memoryIncrementMode    <<  DMA_CR_MINC_Pos;
+	hdma->instance->CR |= hdma->config.circularMode	<< DMA_CR_CIRC_Pos;
+	hdma->instance->CR |= hdma->config.PSIZE << DMA_CR_PSIZE_Pos;
+	hdma->instance->CR |= hdma->config.MSIZE << DMA_CR_MSIZE_Pos;
+}
 
 
